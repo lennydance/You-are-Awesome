450db4ff0362208733a9df794330b5cfc1aa27e6
Lenny Dance <44321077+lennydance@users.noreply.github.com>
2024-02-25 16:54:58 -0800

    Added imageView

--- You are Awesome/ViewController.swift
+++ You are Awesome/ViewController.swift
@@ -9,16 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("👍 View did load has run!")
-        messageLabel.text="Fabulous, That's You!"
+
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
+
         messageLabel.text="You Are Awesome!"
+        imageView.image = UIImage(named: "image0")
     }
     
 }
